be4a2d21edff0d887a8025ee725e8e386c5f3346
Krzysztof <dreelleen@gmail.com>
2023-03-28 23:11:14 +0200

    Addes auto swiper section, auto carousel section beginning work

---
+++ scripts/swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('#auto-swiper', {
+    loop: true,
+    autoplay: {
+      pauseOnMouseEnter: true,
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  const carouselSwiper = new Swiper('#carousel-swiper', {
+    loop: true,
+    autoplay: {
+      pauseOnMouseEnter: true,
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
