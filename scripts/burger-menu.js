ca17d606c70d7ae320202fa95872a82e2f0fdf05
Krzysztof <dreelleen@gmail.com>
2023-04-07 22:02:21 +0200

    Side navigation work started

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -1,14 +1,36 @@
 const burgerButtons = document.getElementsByClassName("nav-burger-wrapper");
-const sideNavigation = document.getElementsByClassName("side-navigation");
 const burgerCloseButtons = document.getElementsByClassName("side-navigation-close");
+
+const sideNavigation = document.getElementsByClassName("side-navigation");
+const sideNavigationContent = document.getElementsByClassName("side-navigation-content");
+const sideNavigationComputedStyle = window.getComputedStyle(sideNavigation[0]);
 
 // adjust for full screen side navigation
 for(var i = 0; i < burgerButtons.length; i++){
     burgerButtons[i].addEventListener("click", function() {
+        sideNavigation[0].style.transition = "1s";
+        sideNavigationContent[0].style.transition = "1.5s"
+
         sideNavigation[0].style.left = "0px";
+        sideNavigationContent[0].style.left = "0px";
     })
 };
 
 burgerCloseButtons[0].addEventListener("click", function(){
-    sideNavigation[0].style.left = "-400px";
+    // revere transition time of side navigation and side navigation content in order for side navigation content to hide first
+    sideNavigation[0].style.transition = "2.5s";
+    sideNavigationContent[0].style.transition = "1.5s"
+
+    sideNavigationContent[0].style.left = "-400px";
+    sideNavigation[0].style.left = "-110%";
+});
+
+window.addEventListener("resize", function() {
+    if (this.window.innerWidth > 1280){
+        sideNavigation[0].style.transition = "0s";
+        sideNavigationContent[0].style.transition = "0s"
+
+        sideNavigationContent[0].style.left = "-400px";
+        sideNavigation[0].style.left = "-110%";
+    }
 });
